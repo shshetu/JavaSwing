f7ae3eb167b39813fd4db74be564ceb20616dd06
shshetu <shshetu2017@gmail.com>
2019-02-19 19:03:29 +0600

    java

---
+++ ClassLectures/SwingClassLectures/src/Date_19_02_2019/Evidence/FileReadWrite.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Date_19_02_2019.Evidence;
+
+/**
+ *
+ * @author User
+ */
+public class FileReadWrite {
+    
+}
---
+++ ClassLectures/SwingClassLectures/src/Date_19_02_2019/Evidence/Student.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Date_19_02_2019.Evidence;
+
+/**
+ *
+ * @author User
+ */
+public class Student {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCcourse() {
+        return ccourse;
+    }
+
+    public void setCcourse(String ccourse) {
+        this.ccourse = ccourse;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    private String name;
+    private String age;
+    private String email;
+    private String gender;
+    private String ccourse;
+    private String round;
+    private String comment;
+    
+}
